1b1f984d01199f5a17ea131e66d881a5e48992ed
Joel Mitchell <joelm1988@hotmail.com>
2017-09-12 19:43:57 +1000

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$("#toggle").click(function() {
+  $(this).toggleClass("on");
+  $("#toggle-menu").fadeToggle();
+  $(".north-logo").fadeToggle();
+  $(".nav").toggleClass("on");
+  $("html, body").toggleClass("no-scroll");
+});
+/*
+$(function() {
+   $(window).scroll(function () {
+      if ($(this).scrollTop() > $(window).height()*1) {
+         $(".nav").addClass('show');
+      }
+      if ($(this).scrollTop() < $(window).height()*0.5) {
+         $(".nav").removeClass('show');
+      }
+   });
+});
+*/
